6e05d9d3a7783ee25cb99458a39e9ce5e1b482d6
Ann9510 <lpiyg@naver.com>
2024-02-08 18:51:49 +0900

    20강 완료
    - Toast box를 props로 변경해보기.

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -2,37 +2,41 @@ import logo from "./logo.svg";
 import "./App.css";
 import TextComponent from "./components/TextComponent";
 import Section from "./components/Section";
+import Toast from "./components/Toast";
 
 function App() {
+  const messageArray = [
+    {
+      title: "success",
+      text: "Right on! Your account has been updated.",
+    },
+    {
+      title: "success",
+      text: "Right on! Your account has been updated.",
+    },
+    {
+      title: "warning",
+      text: "Hmmm. Something doesn't look right",
+    },
+    {
+      title: "warning",
+      text: "Hmmm. Something doesn't look right",
+    },
+    {
+      title: "error",
+      text: "Uh oh! Something went terribly wrong!",
+    },
+    {
+      title: "error",
+      text: "Uh oh! Something went terribly wrong!",
+    },
+  ];
+
   return (
     <div className="wrapper">
-      <div className="toast toast-success">
-        <main className="toast__message">
-          <header className="toast__message-title">Success</header>
-          <p className="toast__message-text">
-            Right on! Your account has been updated.
-          </p>
-        </main>
-        <button className="toast__button">Dismiss</button>
-      </div>
-      <div className="toast toast-warning">
-        <main className="toast__message">
-          <header className="toast__message-title">Warning</header>
-          <p className="toast__message-text">
-            Hmmm. Something doesn't look right.
-          </p>
-        </main>
-        <button className="toast__button">Dismiss</button>
-      </div>
-      <div className="toast toast-error">
-        <main className="toast__message">
-          <header className="toast__message-title">Error</header>
-          <p className="toast__message-text">
-            Uh oh! Something went terribly wrong!
-          </p>
-        </main>
-        <button className="toast__button">Dismiss</button>
-      </div>
+      {messageArray.map((message) => (
+        <Toast title={message.title} text={message.text} />
+      ))}
     </div>
   );
 }
