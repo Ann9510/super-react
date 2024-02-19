57f6a0a5d18e16cec24e4992a550e40812d029e3
Ann9510 <lpiyg@naver.com>
2024-02-19 17:23:40 +0900

    26강 완료
    - state 적용을 위한 text 변수 생성

--- react-practice/src/components/Toast.js
+++ react-practice/src/components/Toast.js
@@ -3,13 +3,16 @@ import "./Toast.css";
 import ToastMessage from "./ToastMessage";
 
 const Toast = ({ message }) => {
+  let text = "sample";
   const buttonClickHandler = (title) => {
+    text = "changed!";
     console.log(title);
   };
 
   return (
     <div className={`toast toast-${message.title}`}>
       <ToastMessage message={message} />
+      <p>{text}</p>
       <button
         className="toast__button"
         onClick={() => buttonClickHandler(message.title)}
