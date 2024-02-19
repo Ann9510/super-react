bb85696f59a35679d93819ba71d04be69c423d47
Ann9510 <lpiyg@naver.com>
2024-02-19 17:47:33 +0900

    27강 완료
    - useState 사용하는 예제(text)
    - 주의 예제(부모 컴포넌트 useState 할 경우 자식 컴포넌트들이 리렌더링됨. title)

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,27 +1,37 @@
-import './App.css';
-import Toast from './components/Toast';
+import { useState } from "react";
+import "./App.css";
+import Toast from "./components/Toast";
 
 function App() {
   const messageArray = [
     {
-      title: 'success',
-      text: 'Right On! Your account has been updated.'
+      title: "success",
+      text: "Right On! Your account has been updated.",
     },
     {
-      title: 'warning',
-      text: 'Hmmm. Something doesn\'t look right.'
+      title: "warning",
+      text: "Hmmm. Something doesn't look right.",
     },
     {
-      title: 'error',
-      text: 'Un oh! Something went terribly wrong!'
-    }
+      title: "error",
+      text: "Un oh! Something went terribly wrong!",
+    },
   ];
+
+  const [title, setTitle] = useState("hello");
+  // 부모 컴포넌트 useState하면 자식 컴포넌트들이 리렌더링 되는 예제
 
   return (
     <div className="wrapper">
-      {messageArray.map((message) =>
+      <p>{title}</p>
+      {/* useState를 위한 예제(text) */}
+
+      <button onClick={() => setTitle("changed!")}>클릭하기</button>
+      {/* 부모 컴포넌트 useState하면 자식 컴포넌트들이 리렌더링 되는 예제 */}
+
+      {messageArray.map((message) => (
         <Toast message={message} />
-      )}
+      ))}
     </div>
   );
 }
--- react-practice/src/components/Toast.js
+++ react-practice/src/components/Toast.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Toast.css";
 import ToastMessage from "./ToastMessage";
 
 const Toast = ({ message }) => {
-  let text = "sample";
+  // const [text, setText] = useState("sample");
+  // useState를 위한 예제(text)
+
+  console.log("toast component is executed");
+  // 부모 컴포넌트 useState하면 자식 컴포넌트들이 리렌더링 되는 예제
+
   const buttonClickHandler = (title) => {
-    text = "changed!";
+    // setText("changed!");
+    // useState를 위한 예제(text)
     console.log(title);
   };
 
   return (
     <div className={`toast toast-${message.title}`}>
       <ToastMessage message={message} />
-      <p>{text}</p>
+      {/* <p>{text}</p> */}
+      {/* useState를 위한 예제(text) */}
       <button
         className="toast__button"
         onClick={() => buttonClickHandler(message.title)}
