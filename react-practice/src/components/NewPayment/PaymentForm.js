36a3e41ecf4cc3467517a8226a6c282090b019ea
Ann9510 <lpiyg@naver.com>
2024-02-19 18:16:46 +0900

    28강 -1
    - PaymentForm.js (각각의 state)

--- react-practice/src/components/NewPayment/PaymentForm.js
+++ react-practice/src/components/NewPayment/PaymentForm.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './PaymentForm.css';
+import "./PaymentForm.css";
 
 const PaymentForm = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [today, setToday] = useState(null);
+
+  const inputTypeTextChangeHandler = (event) => {
+    setName(event.target.value);
+  };
+  const inputTypeNumberChangeHandler = (event) => {
+    setPrice(event.target.value);
+  };
+  const inputTypeDateChangeHandler = (event) => {
+    setToday(event.target.value);
+  };
+  const buttonSubmitHandler = () => {
+    console.log("name", name);
+    console.log("price", price);
+    console.log("today", today);
+  };
+
   return (
     <form>
-      <div className='new-payment__controls'>
-        <div className='new-payment__control'>
+      <div className="new-payment__controls">
+        <div className="new-payment__control">
           <label>이름</label>
-          <input type='text' />
+          <input
+            type="text"
+            value={name}
+            onChange={inputTypeTextChangeHandler}
+          />
         </div>
-        <div className='new-payment__control'>
+        <div className="new-payment__control">
           <label>금액</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={price}
+            onChange={inputTypeNumberChangeHandler}
+          />
         </div>
-        <div className='new-payment__control'>
+        <div className="new-payment__control">
           <label>날짜</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2024-12-31"
+            value={today}
+            onChange={inputTypeDateChangeHandler}
+          />
         </div>
       </div>
-      <div className='new-payment__actions'>
-        <button type='submit'>결제 추가</button>
+      <div className="new-payment__actions">
+        <button type="button" onClick={buttonSubmitHandler}>
+          결제 추가
+        </button>
       </div>
     </form>
   );
