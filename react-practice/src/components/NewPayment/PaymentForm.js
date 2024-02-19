77483a9b34c7c65087da65a154b693b6e9daa55d
Ann9510 <lpiyg@naver.com>
2024-02-19 19:47:58 +0900

    29강 완료
    - form 제출(submit)
    - event.preventDefault() 추가
    - Two-way binding

--- react-practice/src/components/NewPayment/PaymentForm.js
+++ react-practice/src/components/NewPayment/PaymentForm.js
@@ -6,39 +6,46 @@ const PaymentForm = () => {
   const [objectState, setObjectState] = useState({
     name: "",
     price: 0,
-    today: null,
+    today: new Date(),
   });
 
-  const inputTypeTextChangeHandler = (event) => {
+  const inputTextHandler = (event) => {
     setObjectState((prevState) => ({ ...prevState, name: event.target.value }));
   };
-  const inputTypeNumberChangeHandler = (event) => {
+  const inputPriceHandler = (event) => {
     setObjectState((prevState) => ({
       ...prevState,
       price: event.target.value,
     }));
   };
-  const inputTypeDateChangeHandler = (event) => {
+  const inputTodayHandler = (event) => {
     setObjectState((prevState) => ({
       ...prevState,
       today: event.target.value,
     }));
   };
-  const buttonSubmitHandler = () => {
-    console.log("name", objectState.name);
-    console.log("price", objectState.price);
-    console.log("today", objectState.today);
+
+  const buttonSubmitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(objectState);
+
+    setObjectState({
+      name: "",
+      price: 0,
+      today: new Date(),
+    });
   };
 
   return (
-    <form>
+    <form onSubmit={buttonSubmitHandler}>
       <div className="new-payment__controls">
         <div className="new-payment__control">
           <label>이름</label>
           <input
             type="text"
             value={objectState.name}
-            onChange={inputTypeTextChangeHandler}
+            onChange={inputTextHandler}
           />
         </div>
         <div className="new-payment__control">
@@ -48,7 +55,7 @@ const PaymentForm = () => {
             min="0.01"
             step="0.01"
             value={objectState.price}
-            onChange={inputTypeNumberChangeHandler}
+            onChange={inputPriceHandler}
           />
         </div>
         <div className="new-payment__control">
@@ -58,14 +65,12 @@ const PaymentForm = () => {
             min="2019-01-01"
             max="2024-12-31"
             value={objectState.today}
-            onChange={inputTypeDateChangeHandler}
+            onChange={inputTodayHandler}
           />
         </div>
       </div>
       <div className="new-payment__actions">
-        <button type="button" onClick={buttonSubmitHandler}>
-          결제 추가
-        </button>
+        <button type="submit">결제 추가</button>
       </div>
     </form>
   );
