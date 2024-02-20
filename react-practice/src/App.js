a732c3d1599f86bb31b345c23f0f2b357cefa586
Ann9510 <lpiyg@naver.com>
2024-02-20 15:19:51 +0900

    42강 완료
    - 이벤트 버블링
    - 이벤트 버블링 멈추기(event.stopPropagation())

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section onClick={() => console.log("section click")}>
+      <div
+        onClick={(event) => {
+          event.stopPropagation();
+          console.log("div click");
+        }}
+      >
+        <button onClick={() => console.log("button click")}>클릭</button>
+      </div>
+    </section>
   );
 }
 
