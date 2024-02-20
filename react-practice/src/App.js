832b3c05924e5746429fb73144dd0ad7e745a8c8
Ann9510 <lpiyg@naver.com>
2024-02-20 16:01:00 +0900

    43강 완료
    - 드롭다운 컴포넌트 생성

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,18 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Dropdown from "./components/Dropdown";
 
 function App() {
+  const options = [
+    { label: "빨강", value: "red" },
+    { label: "녹색", value: "green" },
+    { label: "파랑", value: "blue" },
+  ];
+
   return (
-    <section onClick={() => console.log("section click")}>
-      <div
-        onClick={(event) => {
-          event.stopPropagation();
-          console.log("div click");
-        }}
-      >
-        <button onClick={() => console.log("button click")}>클릭</button>
-      </div>
-    </section>
+    <Dropdown options={options} />
+
+    // 42강
+    // <section onClick={() => console.log("section click")}>
+    //   <div
+    //     onClick={(event) => {
+    //       event.stopPropagation();
+    //       console.log("div click");
+    //     }}
+    //   >
+    //     <button onClick={() => console.log("button click")}>클릭</button>
+    //   </div>
+    // </section>
   );
 }
 
